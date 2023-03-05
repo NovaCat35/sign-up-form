f72e5cee929ad51148442743c7f16dc3d7e465db
Robert Chen <bergintonatcapital@gmail.com>
2023-03-05 01:38:47 -0500

    Add images and resize/make font responsive

--- README.md
+++ README.md
@@ -1,4 +1,22 @@
+# Sign-up Form
 
+A basic sign-up form created using HTML, CSS, Javascript. It contains validation checking and implements the best validation behavior response base on user's input (see tip below). So...sign up now!
 
-TIP: A twitter thread about making good sign-up forms
+[Click me!](https://novacat35.github.io/sign-up-form/) 📝
+
+> TIP: A twitter thread about making good sign-up forms
 https://twitter.com/vponamariov/status/1380182211576664067
+
+Image Sources:
+Aperture logo: https://commons.wikimedia.org/wiki/File:Aperture_Laboratories_Logo.svg
+
+Portal 2 concept arts: 
+https://isabelle_lemay.artstation.com/projects/XKo8R 
+https://www.artstation.com/artwork/vEwY6
+
+Boats: https://www.artstation.com/artwork/baYK1r
+
+Bridge: https://www.artstation.com/artwork/1nD1Yo
+
+Link's raft: https://www.artstation.com/artwork/4XkGmn
+
--- script.js
+++ script.js
@@ -7,13 +7,12 @@ inputs.forEach(input => input.addEventListener('focus', (event) => {
 
 
 inputs.forEach(input => input.addEventListener('focusout', (event) => {
-    console.log(event);
     let input = event.target;
     let inputID = event.target.id;
     let label = event.target.previousElementSibling;
 
-    // add 'active' attr to class for any tampered input (after user focus-out of input)
-    // this is so we don't aggressively tell user they have error right from the get-go but only 
+    // Add 'active' attr to class for any tampered input (after user focus-out of input).
+    // This is so we don't aggressively tell user they have error right from the get-go but only 
     // when they go back into focus to correct the mistake, haha :9
     input.classList.add('active');
 
@@ -27,7 +26,30 @@ function placeholderActive(selector) {
     return document.querySelector('#' + selector + ':placeholder-shown');
 }
 
-// password confirmation checker
+// --- Password Confirmation Checker ---
+let passInput1 = document.querySelector('#password1');
+let passInput2 = document.querySelector('#password2');
+
+password2.addEventListener('input', () => {
+    checkPasswords(passInput1.value, passInput2.value);
+    if (passInput2.validity.valid) {
+        console.log('VALID PASS');
+        passInput2.classList.remove('invalidPassword');
+    } else {
+        console.log('INVALID PASS');
+        passInput2.classList.add('invalidPassword');
+    }
+});
+
+function checkPasswords(p1, p2) {
+    if(p1 !== p2){
+        console.log('ERROR');
+        passInput2.setCustomValidity(' ');
+    } else {
+        passInput2.setCustomValidity("");
+    }
+    passInput2.reportValidity();
+}
 
 
 
